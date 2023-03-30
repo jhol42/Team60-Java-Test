17ca5a5c3fba8f205bfb9150f8384c56509d835d
Justin Holzer <justin@jtcholzer.net>
2023-03-30 07:02:05 -0700

    Add LimeLight NetworkTables skeleton.

--- src/main/java/frc/robot/subsystems/LimeLight.java
+++ src/main/java/frc/robot/subsystems/LimeLight.java
@@ -1,5 +1,32 @@
 package frc.robot.subsystems;
 
-public class LimeLight {
-    
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class LimeLight extends SubsystemBase {
+    private NetworkTable table;
+
+    public LimeLight() {
+        table = NetworkTableInstance.getDefault().getTable("limelight");
+    }
+
+    @Override
+    public void periodic() {
+        NetworkTableEntry tx = table.getEntry("tx");
+        NetworkTableEntry ty = table.getEntry("ty");
+        NetworkTableEntry ta = table.getEntry("ta");
+
+        // read values periodically
+        double x = tx.getDouble(0.0);
+        double y = ty.getDouble(0.0);
+        double area = ta.getDouble(0.0);
+
+        // post to smart dashboard periodically
+        SmartDashboard.putNumber("LimelightX", x);
+        SmartDashboard.putNumber("LimelightY", y);
+        SmartDashboard.putNumber("LimelightArea", area);
+    }
 }
