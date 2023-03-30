d1c442d05aee22d2b4c485e37b106b3c1d79e67a
Justin Holzer <justin@jtcholzer.net>
2023-03-30 07:29:56 -0700

    Add NavX skeleton.

--- src/main/java/frc/robot/subsystems/NavX.java
+++ src/main/java/frc/robot/subsystems/NavX.java
@@ -1,5 +1,29 @@
 package frc.robot.subsystems;
+import com.kauailabs.navx.frc.AHRS;
 
-public class NavX {
-    
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.SPI;
+
+public class NavX extends SubsystemBase {
+    AHRS ahrs;
+    public NavX(){
+        try {
+            /***********************************************************************
+             * navX-MXP: - Communication via RoboRIO MXP (SPI, I2C) and USB. - See
+             * http://navx-mxp.kauailabs.com/guidance/selecting-an-interface.
+             * 
+             * navX-Micro: - Communication via I2C (RoboRIO MXP or Onboard) and USB. - See
+             * http://navx-micro.kauailabs.com/guidance/selecting-an-interface.
+             * 
+             * VMX-pi: - Communication via USB. - See
+             * https://vmx-pi.kauailabs.com/installation/roborio-installation/
+             * 
+             * Multiple navX-model devices on a single robot are supported.
+             ************************************************************************/
+                ahrs = new AHRS(SPI.Port.kMXP);
+            } catch (RuntimeException ex) {
+                DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
+            }
+    }
 }
