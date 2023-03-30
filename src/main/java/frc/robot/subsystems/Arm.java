0287fc115cd42f726a1fa80f0ea6c32a94bfc7c6
Justin Holzer <justin@jtcholzer.net>
2023-03-30 06:17:48 -0700

    Arm Submodule work.

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,5 +1,34 @@
 package frc.robot.subsystems;
 
-public class Arm {
-    
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import java.util.function.DoubleSupplier;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Arm extends SubsystemBase {
+
+    private static int BlueMotorCanId = 3;
+    private static int RedMotorCanId = 4;
+    private static int LimitSwitchDIOPort = 9;
+
+    private CANSparkMax BlueMotor;
+    private CANSparkMax RedMotor;
+    private Encoder BlueEncoder;
+    private Encoder RedEncoder;
+    private DigitalInput LimitSwitch;
+
+
+    public Arm()
+    {
+        super();
+        BlueMotor = new CANSparkMax(BlueMotorCanId, MotorType.kBrushless);
+        RedMotor = new CANSparkMax(RedMotorCanId, MotorType.kBrushless);
+        LimitSwitch = new DigitalInput(LimitSwitchDIOPort);
+    }
 }
